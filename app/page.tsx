30cea6e0440fab195d93d6b1dc61935a93133faa
umay7 <umar.mustaqim182@gmail.com>
2024-12-09 23:58:31 +0700

    ok

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+"use client"
+
+import Game from "../game"
+
+export default function SyntheticV0PageForDeployment() {
+  return <Game />
+}
--- utils/sound.ts
+++ utils/sound.ts
@@ -48,4 +48,3 @@ class SoundManager {
 }
 
 export const soundManager = new SoundManager();
-
